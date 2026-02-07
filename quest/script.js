deb102368242b8040d97e133b4bde5662b0537da
LAU RICHARD <richardlau1167@gmail.com>
2026-02-07 03:22:02 -0500

    feat: enhance scene transitions with fade-in/out animations and update audio interactions

--- quest/script.js
+++ quest/script.js
@@ -1,17 +1,58 @@
-function hideBox(id) {
-  const el = document.getElementById(id);
+//fade animation
 
-  el.classList.add("fade-out");
+const scenes = document.querySelectorAll(".scene");
+let current = 0;
 
-  setTimeout(() => {
-    el.style.display = "none";
-  }, 500);
+function nextScene() {
+  scenes[current].classList.remove("active");
+
+  current++;
+
+  if (current < scenes.length) {
+    scenes[current].classList.add("active");
+  }
 }
 
 
+//music for bg
 const audio = document.getElementById("bgMusic");
 
 document.addEventListener("click", () => {
-    audio.volume = 0.5;
+    audio.volume = 0.1;
     audio.play();
 }, { once: true });
+
+//select sound
+const selectSound = document.getElementById("selectSound");
+const select = document.querySelectorAll(".select");
+
+// Add click sound to each .select element
+select.forEach(btn => {
+    btn.addEventListener("click", () => {
+        selectSound.volume = 0.1;
+        selectSound.currentTime = 0; // allow replay
+        selectSound.play();
+    });
+});
+
+//typewriter
+
+const elements = document.querySelectorAll(".typewriter");
+const speed = 30; // ms per character
+
+elements.forEach(el => {
+    const text = el.textContent;
+    el.textContent = "";
+
+    let i = 0;
+
+    function type() {
+        if (i < text.length) {
+            el.textContent += text.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        }
+    }
+
+    type();
+});
